29a705910359e708b59a46568b8709d69f551178
draftmanpenny <94030281+draftmanpenny@users.noreply.github.com>
2022-08-31 16:08:22 -0700

    Update views.py

--- search/views.py
+++ search/views.py
@@ -12,9 +12,9 @@ def index(request):
 
 def news(request, article): 
     if request.method == 'POST':
-        url = "https://google-finance4.p.rapidapi.com/search/"
+        url = "https://google-finance4.p.rapidapi.com/ticker/"
 
-        querystring = {"q":"airbnb","hl":"en","gl":"US"}
+        querystring = {"t":"LIT-USD","hl":"en","gl":"US"}
 
         headers = {
             "X-RapidAPI-Key": "31c5541e87msh0684494d7f7396fp117984jsn574856ff6d0c",
@@ -22,10 +22,14 @@ def news(request, article):
         }
 
         response = requests.request("GET", url, headers=headers, params=querystring)
+        response.status_code
+        response.text
+        response.json()
+        articles = response.json()['news', 'price']
 
        
 
         return render(request, 'news.html', {
-             "article": response })  
+             "article": articles })  
     else:
         return HttpResponse('Error')
